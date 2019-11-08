a8b44f29096495010fc189811dcbe7de00fe54fc
rhino797 <rhino797@users.noreply.github.com>
2019-11-08 13:26:13 +0100

    Added JSON formatting for BAT & WEAT

--- js/evaluateSetOne.js
+++ js/evaluateSetOne.js
@@ -5,17 +5,17 @@ elementID_2 = 'table_2';
 window.onload = doByStart;
 
 function doByStart() {
-  loadTestData(jsonFile_1, elementID_1);
-  loadTestData(jsonFile_2, elementID_2);
+    loadTestData(jsonFile_1, elementID_1);
+    loadTestData(jsonFile_2, elementID_2);
 }
 
 function loadTestData(jsonFile, elementID) {
     fetch(jsonFile)
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data);
-    let output = "<a></a>";
-    output += `
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data);
+            let output = "<a></a>";
+            output += `
     <tbody>
     <tr>
       <th scope="row">T1</th>
@@ -35,8 +35,8 @@ function loadTestData(jsonFile, elementID) {
     </tr>
     </tbody>
     `;
-    document.getElementById(elementID).innerHTML = output;
-  })
+            document.getElementById(elementID).innerHTML = output;
+        })
 }
 
 document.getElementById('Set1_Evaluate').addEventListener('click', sendRequest);
@@ -50,32 +50,39 @@ function sendRequest() {
     argSet1 = "caress freedom health love peace cheer friend heaven loyal pleasure diamond gentle honest lucky rainbow diploma gift honor miracle sunrise family happy laughter paradise vacation";
     argSet2 = "abuse crash filth murder sickness accident death grief poison stink assault disaster hatred pollute tragedy divorce jail poverty ugly cancer kill rotten vomit agony prison";
     const url = 'http://127.0.0.1:5000/REST/get_bias_evaluations';
-    dataJSON = {T1:testSet1, T2:testSet2, A1:argSet1, A2:argSet2};
+    dataJSON = { T1: testSet1, T2: testSet2, A1: argSet1, A2: argSet2 };
 
     try {
         const response = fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(dataJSON),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })    
-        .then((res) => res.json())
-        .then((data) => {
-            let output;
-            output += `
+                method: 'POST',
+                body: JSON.stringify(dataJSON),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                let output;
+                output += `
             <ul>
                 <a>ECT with argument set 1: ${data.ect_value1}</a>
                 <br>
+                <a>ECT p-value with argument set 1: ${data.p_value1}</a>
+                <br>
                 <a>ECT with argument set 2: ${data.ect_value2}</a>
                 <br>
+                <a>ECT p-value with argument set 2: ${data.p_value2}</a>
+                <br>
+                <a>BAT score: ${data.bat_result}</a>
+                <br>
+                <a>WEAT effect size: ${data.weat_effect_size}</a>
+                <br>
+                <a>WEAT p-value: ${data.weat_pvalue}</a>
             </ul>  
-            `;            
-        document.getElementById('card_words_response').innerHTML = output;
-        })
-    } 
-    catch (error) {
+            `;
+                document.getElementById('card_words_response').innerHTML = output;
+            })
+    } catch (error) {
         console.error();
     }
 }
-
