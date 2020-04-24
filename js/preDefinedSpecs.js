eb3edba30b3ca6eded89e896f9804ffe02f5ca58
nfriedri <nfriedri@users.noreply.github.com>
2020-04-24 21:35:19 +0200

    Added automized specs loading

---
+++ js/preDefinedSpecs.js
@@ -0,0 +1,96 @@
+const element = document.getElementById('tables')
+const numberOfResources = 10;
+const resourcePath = '/res/test_set';
+const resourceType = '.json';
+const tables = '';
+var tableNameArray = [];
+var tableIconArray = [];
+var tableContentArray = [];
+var tableButtonArray = [];
+
+async function loadTestData() {   
+    console.log('TestData Loading')
+    for (i=1; i<=numberOfResources; i++){
+        console.log(i)
+        var filePath = resourcePath + i + resourceType;
+        var tableName = 'table' + i;
+        var tableIcon = 'table' + i + 'icon';
+        var tableContent = 'table' + i + 'content';
+        var tableButton = 'table' + i + 'button';
+        var tableScript = 'table' + i + 'script';
+        console.log(tableName +' '+ tableIcon + ' ' + tableContent + " " + tableButton)
+        await fetch(filePath)
+        .then(response => response.json())
+        .then((data) => {
+            let output = "";
+            output += `
+            <div class="my-4">
+                <table class="table table-borderless table-dark my-0">
+                    <thead>
+                        <tr>
+                            <th scope="col" id="${tableIcon}"><a class="btn" role="button"> <i class="icon-angle-down"></i> </a></th>
+                            <th scope="col" id="${tableName}">${data.Name}</th>
+                        </tr>
+                    </thead>
+                    <tbody id="${tableContent}" hidden>
+                        <tr>
+                            <th scope="row">T1</th>
+                            <td>${data.T1}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">T2</th>
+                            <td>${data.T2}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">A1</th>
+                            <td>${data.A1}</td> 
+                        </tr>
+                        <tr>
+                            <th scope="row">A2</th>
+                            <td>${data.A2}</td> 
+                        </tr>
+                    </tbody> 
+                </table>
+                <div class="container my-0" id="${tableButton}" style="background-color: #212429;" hidden>
+                    <button type="button" class="btn btn-primary btn-lg my-2" style="background-color: #222E58; border-color: #FFFFFF;" id="selectSpec">Select</button> 
+                </div>
+            </div>
+            <script>
+                function show(content, button, icon){
+                    console.log('Clicked in function');
+                    var element = document.getElementById(content);
+                    var btn = document.getElementById(button);
+                    var iconn = document.getElementById(icon);
+                    if (element.getAttribute('hidden') == null) {
+                        element.setAttribute('hidden', 'true');  
+                        btn.setAttribute('hidden', 'true');
+                        iconn.innerHTML =  \`<a class="btn" role="button"> <i class="icon-angle-down"></i> </a>\`;        
+                        return 1;
+                    }
+                    else {
+                        element.removeAttribute('hidden');
+                        btn.removeAttribute('hidden');
+                        iconn.innerHTML =  \`<a class="btn" role="button"> <i class="icon-angle-right"></i> </a>\`;
+                        return 0;
+                    }
+                }
+                document.getElementById("${tableName}").addEventListener("click", function() {show("${tableContent}", "${tableButton}", "${tableIcon}")});
+                document.getElementById("${tableIcon}").addEventListener("click", function() {show("${tableContent}", "${tableButton}", "${tableIcon}")});
+            </script>
+            <script>
+                var x = document.getElementById($)
+                console.log('WTF');
+            </script>
+            `;
+            element.innerHTML += output;
+        })
+    }
+    var scripts = element.getElementsByTagName("script");
+    for (var i = 0; i < scripts.length; i++) {
+        eval(scripts[i].innerText);
+    }
+}
+
+window.onload = loadTestData();
+
+
